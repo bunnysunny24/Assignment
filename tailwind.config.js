5c6274ce752d7987581410b135845fd912f37910
bunnysunny24 <bhavashesh@gmail.com>
2025-04-11 22:40:19 +0530

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure this matches your file structure
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], 
   theme: {
     extend: {},
   },
